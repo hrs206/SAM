ddcb93818fb1576931c6b6acdffe9563077b6344
hrs206 <129712709+hrs206@users.noreply.github.com>
2023-05-13 23:22:23 +0530

    Update settings.py

--- SAM/SAM/settings.py
+++ SAM/SAM/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-=u0c*%2(sbdh4%ym8l2o$*ygf(#(0l9dohp9nw(1&xi0iqa9tp"
+SECRET_KEY = "django"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -80,7 +80,7 @@ DATABASES = {
         "NAME": "SAM",
         "HOST": "localhost",
         "USER": "root",
-        "PASSWORD": "ilsql@hrs2001"
+        "PASSWORD": "password"
     }
 }
 
